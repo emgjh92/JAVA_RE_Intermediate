693d2729afd4c24c052e4e6521c57a4c51625d8f
Choi jaehyun <emgjh92@naver.com>
2020-03-20 16:50:28 +0900

    2020-03-20
    2020-03-20

--- P20200320_학생관리프로그램/src/useclass/Main.java
+++ P20200320_학생관리프로그램/src/useclass/Main.java
@@ -2,7 +2,11 @@ package useclass;
 
 public class Main {
 	public static void main(String[] args) {
-			
+/*
+static 변수(정적 변수) 는 분명 global 변수(전역 변수) 와는 다르다.
+하지만, !!Java 언어 한정으로!! static을 활용하여 전역변수의 역할을 수행 하므로,
+static = global 변수 라고 이해할 수 있다.
+*/
 		new STMContorller(new STMModel()).run();
 	}
 }
