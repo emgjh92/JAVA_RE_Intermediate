a4b1cee85ea6ffa0cc0430677781a1c4527183cf
Choi jaehyun <emgjh92@naver.com>
2020-03-24 09:15:48 +0900

    2020-03-23
    2020-03-23

--- P20200323_자료구조/src/Collection/LowCoupling.java
+++ P20200323_자료구조/src/Collection/LowCoupling.java
@@ -7,7 +7,7 @@ public class LowCoupling {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
 		//만약에 ArrayList 에서 모듈을 LinkedList 로 바꾸고 싶다면?
-		//List<String> list = new LinkedList<String>(); 로만 맞추면 된다 ==> !!!!!!낮은 결합도!!!!!!!
+		//List<String> list = new LinkedList<String>(); 로만 맞추면 된다 ==> !!!!!!낮은 결합도!!!!!!! 
 		//이것이 바로 인터페이스의 최대 장점이다. (표준인 List 로 선언 해 놨기 때문에 최소한의 코드 수정으로 모듈이 싹 바뀐다)
 		
 		List<String> list2 = new ArrayList<>();
